f56082476e5fbb77d1fa5354b414384947ce6b61
Gerard4168 <gerard2234506@correo.uis.edu.co>
2023-03-03 15:05:51 -0500

    subir

---
+++ proceso.py
@@ -0,0 +1,20 @@
+# X mayor que Y
+
+print("-------------------------------------------------")
+print("------------X mayor que Y o viseversa------------")
+print("-------------------------------------------------")
+
+#input
+
+x= int(input("Digite el valor de x: "))
+y= int(input("Digite el valor de y: "))
+#Procesing
+
+if x>y : 
+    rta = " X es mayor"
+
+else:
+    rta = " Y es mayor"
+
+
+print("El valor " + str(rta))
